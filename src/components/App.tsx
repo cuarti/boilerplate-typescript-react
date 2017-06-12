c375431c79c24032aff9396d5217b12c20cebf01
cuarti <cuarti1992@gmail.com>
2017-06-12 23:44:55 +0200

    Update NodeJS version to last LTS 6.11.0

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,8 +9,8 @@ export default class App extends React.Component<{}, {}> {
     public render(): JSX.Element {
         return (
             <div className={style.app} >
-                <h1>boilerplate-webpack-react-ts</h1>
-                <p>Full boilerplate for react and typescript with webpack2 and hot module replacement</p>
+                <h1>boilerplate-typescript-react</h1>
+                <p>Full boilerplate for react in typescript with webpack2 and hot module replacement</p>
             </div>
         );
     }
